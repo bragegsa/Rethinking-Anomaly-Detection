103d98570f88c0cdfe24285a8447069cc4793ca3
Brage Gaasø Samsonsen <59958033+bragegsa@users.noreply.github.com>
2022-11-10 16:31:11 +0100

    Small changes

--- dataset.py
+++ dataset.py
@@ -54,6 +54,9 @@ class Dataset:
         elif name == 'amazon':
             dataset = FraudAmazonDataset()
             graph = dataset[0]
+
+            print("number of nodes", len(dataset[0]))
+
             if homo:
                 graph = dgl.to_homogeneous(dataset[0], ndata=['feature', 'label', 'train_mask', 'val_mask', 'test_mask'])
                 graph = dgl.add_self_loop(graph)
